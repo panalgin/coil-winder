43ea185bc1af34865132a843a47d46c1d0f11a93
panalgin <daflan.official@gmail.com>
2019-06-21 00:17:38 +0300

    basic screen layouts

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,42 @@
 #define PEDAL_PIN A7
 #define BUZZER_PIN 13
 
+typedef enum
+{
+  None,
+  Running,
+  Paused,
+  Finished,
+} OperationState;
+
+OperationState OpState;
+
 //define the cymbols on the buttons of the keypads
 char hexaKeys[KEY_ROWS][KEY_COLS] = {
-  {'G', 'H', '#', '*'},
-  {'1', '2', '3', '˄'},
-  {'4', '5', '6', '˅'},
-  {'7', '8', '9', 'E'},
-  {'<', '0', '>', 'R'}
-};
+    {'G', 'H', '#', '*'},
+    {'1', '2', '3', 'F'},
+    {'4', '5', '6', 'E'},
+    {'7', '8', '9', 'D'},
+    {'A', '0', 'B', 'C'}};
 
-byte rowPins[KEY_ROWS] = {6, 7, 8, 9, 10}; //connect to the row pinouts of the keypad
-byte colPins[KEY_COLS] = {2, 3, 4, 5}; //connect to the column pinouts of the keypad
+byte colPins[KEY_COLS] = {2, 3, 4, 5};     //connect to the row pinouts of the keypad
+byte rowPins[KEY_ROWS] = {10, 9, 8, 7, 6}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, KEY_ROWS, KEY_COLS); 
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, KEY_ROWS, KEY_COLS);
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 void watermark();
 void readInputs();
+void updateScreen();
+void printScreen(char lines[LCD_ROWS][LCD_COLS + 1]);
+void showMain();
+void showLive();
+void startWinding();
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(PEDAL_PIN, INPUT_PULLUP);
@@ -47,21 +62,23 @@ void setup() {
   watermark();
 }
 
-void watermark() {
+void watermark()
+{
   char line[4][LCD_COLS + 1] =
-  {
-    {"        KARA        "},
-    {"      ELEKTRONIK    "},
-    {"    SES SISTEMLERI  "},
-    {"      v20/06/19     "}
-  };
+      {
+          {"        KARA        "},
+          {"      ELEKTRONIK    "},
+          {"    SES SISTEMLERI  "},
+          {"      v20/06/19     "}};
 
   lcd.clear();
 
-  for(uint8_t i = 0; i < LCD_ROWS; i++) {
+  for (uint8_t i = 0; i < LCD_ROWS; i++)
+  {
     lcd.setCursor(0, i);
 
-    for(uint8_t j = 0; j < LCD_COLS; j++) {
+    for (uint8_t j = 0; j < LCD_COLS; j++)
+    {
       lcd.print(line[i][j]);
       delay(30);
     }
@@ -70,20 +87,99 @@ void watermark() {
   delay(1000);
 }
 
-void loop() {
+void loop()
+{
   readInputs();
   updateScreen();
 }
 
-void readInputs() {
+void readInputs()
+{
   char key = customKeypad.getKey();
 
-  if (key) {
-    Serial.print(F("Pressed: "));
+  if (key)
+  {
     Serial.println(key);
+
+    switch (OpState)
+    {
+    case OperationState::None:
+    {
+      if (key == 'C') {
+        startWinding();
+      } // Enter
+      break;
+    }
+    }
   }
 }
 
-void updateScreen() {
+bool screenNeedsUpdate = true;
 
+void updateScreen()
+{
+  if (screenNeedsUpdate)
+  {
+    switch (OpState)
+    {
+    case OperationState::None:
+    {
+      showMain();
+      break;
+    }
+
+    case OperationState::Running:
+    {
+      showLive();
+      break;
+    }
+    default:
+      break;
+    }
+
+    screenNeedsUpdate = false;
+  }
+}
+
+void showMain()
+{
+  char lines[LCD_ROWS][LCD_COLS + 1] = {
+      {"SPIR:    610        "},
+      {"TEL CAP: 03.20mm    "},
+      {"F1: MOD: AUTO       "},
+      {"          ENT: BASLA"}};
+
+  printScreen(lines);
+}
+
+void showLive()
+{
+  char lines[LCD_ROWS][LCD_COLS + 1] = {
+      {"TUR:  0 / 610       "},
+      {"HIZ:  30 rpm        "},
+      {"                    "},
+      {"          ESC: IPTAL"}};
+
+  printScreen(lines);
+}
+
+void printScreen(char lines[LCD_ROWS][LCD_COLS + 1])
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+
+  for (uint8_t i = 0; i < LCD_ROWS; i++)
+  {
+    lcd.setCursor(0, i);
+    
+    for(uint8_t j = 0; j < LCD_COLS; j++) {
+      lcd.print(lines[i][j]);
+    }
+  }
+}
+
+void startWinding() {
+  OpState = OperationState::Running;
+  screenNeedsUpdate = true;
+  //Offset, pedal ve motor devrede
 }
